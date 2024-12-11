6704b01c4f8cfa40f62590677376b4eecfb25b21
Eropik <semenovichegor@gmail.com>
2024-12-11 18:38:31 +0300

    Add mouse listeners

--- src/main/java/com/vizor/test/TestFrame.java
+++ src/main/java/com/vizor/test/TestFrame.java
@@ -1,10 +1,10 @@
 package com.vizor.test;
 
-
-
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -13,6 +13,9 @@ import java.util.Arrays;
 import java.util.List;
 import javax.swing.*;
 
+import com.vizor.test.pagination.PaginationHandler;
+import com.vizor.test.pagination.listener.PaginationMouseListener;
+import com.vizor.test.pagination.event.PaginationEvent;
 import com.vizor.test.service.ImageService;
 import com.vizor.test.service.impl.FileServiceImpl;
 import com.vizor.test.service.FileService;
@@ -20,14 +23,18 @@ import com.vizor.test.service.impl.ImageServiceImpl;
 
 public class TestFrame extends JFrame {
 
+    private static final int ITEMS_ROW = 2;
+    private static final int ITEMS_COL = 2;
+    private static final int ITEMS_PER_PAGE = ITEMS_ROW * ITEMS_COL;
 
     private static final ImageService imageService = new ImageServiceImpl();
     private static final FileService fileService = new FileServiceImpl();
 
-    List<File> files = new ArrayList<>();
-    JPanel container = new JPanel();
-    JScrollPane scrollPane;
-    int totalItems;
+    private List<File> files = new ArrayList<>();
+    private final JPanel container = new JPanel();
+    private JScrollPane scrollPane;
+    private PaginationHandler paginationHandler;
+    private int totalItems;
 
     public TestFrame() {
         setSize(1024, 768);
@@ -49,17 +56,50 @@ public class TestFrame extends JFrame {
         JPanel topPanel = createTopPanel();
         add(topPanel, BorderLayout.NORTH);
 
-        container.setLayout(new GridLayout(0,2, 10, 10));
-
+        container.setLayout(new GridLayout(0, ITEMS_COL, 10, 10));
         scrollPane = new JScrollPane(container);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         add(scrollPane, BorderLayout.CENTER);
 
+        paginationHandler = new PaginationHandler(ITEMS_PER_PAGE, totalItems, 4);
 
+        paginationHandler.modifyNextAndPreviousButton((previous, next) -> {
+            previous.setBackground(new Color(36, 42, 90));
+            previous.setForeground(Color.white);
+            previous.setText("Previous");
+            next.setBackground(new Color(36, 42, 90));
+            next.setForeground(Color.white);
+            next.setText("Next");
+        });
 
+        paginationHandler.modifyButton(label -> {
+            label.setOpaque(true);
+            label.setBackground(new Color(113, 122, 170));
+            label.setForeground(Color.BLACK);
+        });
 
-        populate(0, totalItems - 1);
+        paginationHandler.addMouseListener(new PaginationMouseListener() {
+            @Override
+            public void onClick(PaginationEvent e) {
+                populate(e.startIndex(), e.endIndex());
+            }
+
+            @Override
+            public void onSelected(PaginationEvent e) {
+                e.getButton().setBackground(Color.DARK_GRAY);
+                e.getButton().setForeground(Color.white);
+            }
+
+            @Override
+            public void onHover(PaginationEvent e) {
+                e.getButton().setBackground(new Color(80, 94, 179));
+            }
+        });
+
+        add(paginationHandler.getPagination(), BorderLayout.SOUTH);
+
+        populate(0, Math.min(ITEMS_PER_PAGE, totalItems) - 1);
     }
 
     private JPanel createTopPanel() {
@@ -91,35 +131,23 @@ public class TestFrame extends JFrame {
                     fileService.uploadFile(selectedFile, "assets");
                     JOptionPane.showMessageDialog(container, "File uploaded successfully!");
                     refreshFiles();
-                } catch (
-                        IOException ex) {
+                } catch (IOException ex) {
                     JOptionPane.showMessageDialog(container, "Failed to upload the file.", "Error", JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
 
-
-
         return topPanel;
     }
 
     private void refreshFiles() {
-        files = fileService.refreshFiles("assets");
-        totalItems = fileService.getTotalItems();
-
-        populate(0, Math.min(4, totalItems) - 1);
+        File[] fs = fileService.getFilesInFolder("assets");
+        files.clear();
+        files.addAll(Arrays.asList(fs));
+        totalItems = files.size();
+        paginationHandler.setTotalItems(totalItems);
+        populate(0, Math.min(ITEMS_PER_PAGE, totalItems) - 1);
     }
-
-    private void resizeAndShowName(File file, JLabel thumbnailLabel, double scale) {
-        try {
-            BufferedImage resizedImage = imageService.resizeImage(file, scale);
-            ImageIcon resizedIcon = new ImageIcon(resizedImage);
-            thumbnailLabel.setIcon(resizedIcon);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
 
     private void populate(int start, int end) {
         container.removeAll();
@@ -139,16 +167,37 @@ public class TestFrame extends JFrame {
 
                 thumbnailLabel.add(imageNameLabel);
 
-                  container.add(thumbnailLabel);
+                thumbnailLabel.addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        JFrame fullImageFrame = new JFrame(file.getName());
+                        fullImageFrame.setSize(700, 700);
+                        fullImageFrame.add(new JLabel(new ImageIcon(file.getAbsolutePath())));
+                        fullImageFrame.setVisible(true);
+                    }
+
+                    @Override
+                    public void mouseEntered(MouseEvent e) {
+                        resizeAndShowName(file, thumbnailLabel, 1.5);
+                        imageNameLabel.setVisible(true);
+                    }
+
+                    @Override
+                    public void mouseExited(MouseEvent e) {
+                        resizeAndShowName(file, thumbnailLabel, 2);
+                        imageNameLabel.setVisible(false);
+                    }
+                });
+
+                container.add(thumbnailLabel);
             } catch (Exception e) {
-                break;
+                e.printStackTrace();
             }
         }
 
         container.revalidate();
         container.repaint();
     }
-
 
     private void filterAndLoadImages(String query) {
         container.removeAll();
@@ -173,8 +222,18 @@ public class TestFrame extends JFrame {
             }
         }
 
-        container.repaint();
         container.revalidate();
+        container.repaint();
+    }
+
+    private void resizeAndShowName(File file, JLabel thumbnailLabel, double scale) {
+        try {
+            BufferedImage resizedImage = imageService.resizeImage(file, scale);
+            ImageIcon resizedIcon = new ImageIcon(resizedImage);
+            thumbnailLabel.setIcon(resizedIcon);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
